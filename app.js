85b41f6377458b89aa8b3f97ead2d7aacb0732b1
sevlyar <sergey.yarmonov@gmail.com>
2014-09-29 21:39:03 +0700

    Imported style.css and app.js. Implemented the basic functions.

---
+++ app.js
@@ -0,0 +1,42 @@
+Colorhythm.onerror = function(e) {
+	$('.error-msg')
+		.show()
+		.html(e.message);
+};
+
+$('.screen').each(function(i, el) {
+	var canvas = $(el);
+	var scr = Colorhythm.Screen();
+	scr.canvas(el);
+	Colorhythm.loadVisualization(canvas.data('visualization'), scr);
+
+	var fullscreen = $('<a class="button light">enter fullscreen</a>');
+	var onoff = $('<a class="button light">visualization off</a>');
+	canvas.parent().after(fullscreen);
+	fullscreen.after(onoff);
+	fullscreen.after(' ');
+
+	fullscreen.click(function() {
+		canvas.attr('id', 'fullscreen');
+	});
+
+	$(window).keyup(function(e) {
+		if (e.keyCode == 27) {
+			canvas.removeAttr('id');
+		}
+	});
+
+	onoff.click(function() {
+		if (scr._active) {
+			scr.off();
+			onoff.html('visualization on');
+			onoff.toggleClass('light');
+			onoff.toggleClass('dark');
+		} else {
+			scr.on();
+			onoff.html('visualization off');
+			onoff.toggleClass('light');
+			onoff.toggleClass('dark');
+		}
+	});
+});
